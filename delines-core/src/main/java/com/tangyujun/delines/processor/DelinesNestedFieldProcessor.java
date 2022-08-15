9b8d52cafd6b9db77dfd59a31fcc67329a0042a7
ns-cn <ns-cn@qq.com>
2022-08-15 14:29:11 +0800

    DelinesNestedFieldProcessor

---
+++ delines-core/src/main/java/com/tangyujun/delines/processor/DelinesNestedFieldProcessor.java
@@ -0,0 +1,49 @@
+package com.tangyujun.delines.processor;
+
+import cn.hutool.core.text.CharSequenceUtil;
+import com.google.auto.service.AutoService;
+import com.tangyujun.delines.annotation.DelinesField;
+import com.tangyujun.delines.annotation.DelinesNestedField;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.tools.Diagnostic;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+@SupportedAnnotationTypes({"com.tangyujun.delines.annotation.DelinesNestedField"})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@AutoService(Processor.class)
+public class DelinesNestedFieldProcessor extends AbstractProcessor {
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env) {
+		Messager messager = processingEnv.getMessager();
+		messager.printMessage(Diagnostic.Kind.NOTE, "@DelinesNestedField Checking");
+		Set<? extends Element> elements = env.getElementsAnnotatedWith(DelinesNestedField.class);
+		boolean success = true;
+		for (Element element : elements) {
+			if (element.getKind().equals(ElementKind.FIELD)) {
+				VariableElement variableElement = (VariableElement) element;
+				DelinesField field = variableElement.getAnnotation(DelinesField.class);
+				if (field != null) {
+					messager.printMessage(Diagnostic.Kind.ERROR, "could not @DelinesField when @DelinesNestedField!", element);
+					success = false;
+				}
+				DelinesNestedField nestedField = variableElement.getAnnotation(DelinesNestedField.class);
+				if (nestedField != null && CharSequenceUtil.isNotBlank(nestedField.regExp())) {
+					try {
+						Pattern.compile(nestedField.regExp());
+					} catch (Exception e) {
+						messager.printMessage(Diagnostic.Kind.ERROR, "@DelinesNestedField with wrong pattern: " + nestedField.regExp(), element);
+						success = false;
+					}
+				}
+			}
+		}
+		return success;
+	}
+}

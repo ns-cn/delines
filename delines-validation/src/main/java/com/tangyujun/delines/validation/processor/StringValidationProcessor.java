f9193a2502c8606028776ef88fd0954e58e547c5
ns-cn <ns-cn@qq.com>
2022-08-25 15:14:10 +0800

    string validation processor

--- delines-validation/src/main/java/com/tangyujun/delines/validation/processor/StringValidationProcessor.java
+++ delines-validation/src/main/java/com/tangyujun/delines/validation/processor/StringValidationProcessor.java
@@ -1,9 +1,9 @@
 package com.tangyujun.delines.validation.processor;
 
+import cn.hutool.core.text.CharSequenceUtil;
 import com.google.auto.service.AutoService;
-import com.tangyujun.delines.validation.annotation.AssertFalse;
-import com.tangyujun.delines.validation.annotation.AssertTrue;
-import com.tangyujun.delines.validation.annotation.StringLength;
+import com.tangyujun.delines.processor.PatternChecker;
+import com.tangyujun.delines.validation.annotation.*;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
@@ -14,6 +14,7 @@ import javax.tools.Diagnostic;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
 @SupportedAnnotationTypes({
@@ -32,22 +33,48 @@ public class StringValidationProcessor extends AbstractProcessor {
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env) {
 		Messager messager = processingEnv.getMessager();
 		messager.printMessage(Diagnostic.Kind.NOTE, "Validation annotation Checking");
-		Set<? extends Element> assertFalseElements = env.getElementsAnnotatedWith(AssertFalse.class);
-		Set<? extends Element> assertTrueElements = env.getElementsAnnotatedWith(AssertTrue.class);
-		Consumer<Element> elementConsumer = element -> {
+		Set<? extends Element> stringLengthElements = env.getElementsAnnotatedWith(StringLength.class);
+		Set<? extends Element> stringNotEmptyElements = env.getElementsAnnotatedWith(StringNotEmpty.class);
+		Set<? extends Element> stringPatternElements = env.getElementsAnnotatedWith(StringPattern.class);
+		AtomicBoolean success = new AtomicBoolean(true);
+		Consumer<Element> typeCheck = element -> {
 			if (element.getKind().equals(ElementKind.FIELD)) {
-				Boolean isBoolean = Optional.ofNullable(element.asType())
+				Boolean isString = Optional.ofNullable(element.asType())
 						.map(Objects::toString)
-						.map(Boolean.class.getName()::equals)
+						.map(String.class.getName()::equals)
 						.orElse(false);
-				if (!isBoolean) {
+				if (!isString) {
+					success.set(false);
 					messager.printMessage(Diagnostic.Kind.ERROR,
-							"@AssertFalse or @AssertTrue only for Boolean", element);
+							"@StringLength/@StringNotEmpty/@StringPattern only for String", element);
 				}
 			}
 		};
-		assertFalseElements.forEach(elementConsumer);
-		assertTrueElements.forEach(elementConsumer);
+		Consumer<Element> lengthCheck = element -> {
+			if (element.getKind().equals(ElementKind.FIELD)) {
+				StringLength length = element.getAnnotation(StringLength.class);
+				if (length.min() < 0 || length.max() < 0 || length.max() < length.min()) {
+					success.set(false);
+					messager.printMessage(Diagnostic.Kind.ERROR, "@StringLength with illegal range", element);
+				}
+			}
+		};
+		Consumer<Element> patternCheck = element -> {
+			if (element.getKind().equals(ElementKind.FIELD)) {
+				StringPattern pattern = element.getAnnotation(StringPattern.class);
+				if (CharSequenceUtil.isEmpty(pattern.value())) {
+					success.set(false);
+					messager.printMessage(Diagnostic.Kind.ERROR, "@StringPattern without pattern", element);
+				} else {
+					success.set(PatternChecker.check(pattern.value(), messager, element) && success.get());
+				}
+			}
+		};
+		stringLengthElements.forEach(typeCheck);
+		stringLengthElements.forEach(lengthCheck);
+		stringNotEmptyElements.forEach(typeCheck);
+		stringPatternElements.forEach(typeCheck);
+		stringPatternElements.forEach(patternCheck);
 		return true;
 	}
 }
